acf44bfa3c71ac7afc576fcf5651f495c6e10828
José Daniel Arroyo Fernández <arroyofernandez.josedaniel@gmail.com>
2022-03-26 10:52:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/holamundojdaf/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.holamundojdaf
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var txtNombre: EditText
+    private lateinit var btnAceptar: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txtNombre = findViewById(R.id.txtNombre)
+        btnAceptar = findViewById(R.id.btnAceptar)
+
+
+        btnAceptar.setOnClickListener {
+            //Creamos el Intent
+            val intent = Intent(this@MainActivity, SaludoActivity::class.java)
+            //Añadimos al intent la información a pasar entre actividades
+            intent.putExtra("NOMBRE", txtNombre.text.toString())
+            //Iniciamos la nueva actividad
+            startActivity(intent)
+        }
+    }
+}
